7bc2de0cc2f32b16d87c86c7345cfdcc18bf9c07
sabilaakram <sabilaakram1234@gmail.com>
2024-04-08 00:42:30 +0500

    first commit

---
+++ src/components/Progress.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+
+export default function Progress({ text, percentage }) {
+    percentage = percentage ?? 0;
+    return (
+      <div className="progress-container">
+        <div className='progress-bar' style={{ 'width': `${percentage}%` }}>{text} ({`${percentage.toFixed(2)}%`})</div>
+      </div>
+    );
+  }
